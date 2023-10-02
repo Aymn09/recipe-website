fc189d39951d72ac09da6542518c81bdd77e8021
Aymen <aymenabdassameur@gmail.com>
2023-10-02 10:31:57 +0100

    Responsive Design finished

--- script.js
+++ script.js
@@ -1,50 +1,51 @@
 let containers = document.querySelectorAll('.container');
 let instru = document.querySelector('.instru');
 
-containers.forEach( container => {
+containers.forEach(container => {
   let mealImage = container.querySelector('.meal-image');
   let mealName = container.querySelector('.meal-name');
   let mealCat = container.querySelector('.meal-cat');
   let mealTags = container.querySelector('.meal-tags');
   let instruBtn = container.querySelector('.instru-btn');
-  
+
   let url = "https://www.themealdb.com/api/json/v1/1/random.php";
-  
-  
-    fetch(url)
+
+
+  fetch(url)
     .then((response) => response.json())
     .then((data) => {
       let meal = data.meals[0];
-     
+
 
       mealImage.style.backgroundImage = `url(${meal.strMealThumb})`;
       mealName.innerHTML = `${meal.strMeal}`;
       mealCat.innerHTML = `${meal.strCategory}`;
-      
+
       // mealTags.innerHTML = `${meal.strTags}`;
-      if ( meal.strTags == null ) 
-      { 
+      if (meal.strTags == null) {
         mealTags.innerHTML = `No Tags Available`
       } else {
         mealTags.innerHTML = `${meal.strTags}`;
       }
-      
-      
+
+
       instruBtn.addEventListener('click', () => {
         // instru.innerHTML = meal.strInstructions ;
-        
-        
+
+
         instru.innerHTML = `
         <div class="two alt-two instru-title">
           <h1 class='title'>Instructions:</h1>
         </div>
         <p id="meal-instructions">${meal.strInstructions}</p>
         `;
-        instru.scrollIntoView({ behavior: 'smooth' });
-        
-      }) 
-      
+        instru.scrollIntoView({
+          behavior: 'smooth'
+        });
+
+      })
+
     });
-  
-  
+
+
 })
